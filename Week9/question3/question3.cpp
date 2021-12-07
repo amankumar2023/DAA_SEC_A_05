1824151a72112c341ef3009cd7c3e1b477936051
amankumar2023 <87333733+amankumar2023@users.noreply.github.com>
2021-12-07 16:22:02 +0530

    Create question3.cpp

---
+++ Week9/question3/question3.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    freopen("input.txt","r",stdin);
+    freopen("output.txt","w",stdout);
+    int n;
+    cin>>n;
+    vector<int> a(n);
+    for(int i=0;i<n;i++){
+        cin>>a[i];
+    }
+    priority_queue<int, vector<int>, greater<int>> minheap;
+    for(int i=0;i<n;i++){
+        minheap.push(a[i]);
+    }
+    int ans = 0;
+    while(minheap.size()>1){
+        int e1 = minheap.top();
+        minheap.pop();
+        int e2 = minheap.top();
+        minheap.pop();
+        ans += e1+e2;
+        minheap.push(e1 + e2);
+    }
+    cout<<ans;
+    return 0;
+}
